3a35931ab3f12107578d44961d0c3e1d3f270574
yashvida <yashvida.office@gmail.com>
2018-10-07 21:05:23 +0530

    csurf remove and new changes

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
-# CSRF_Synchronizer_Token_Pattern
-Demo Application
+# CSRF Synchronizer Token Pattern - NodeJS
+
+## Installation
+
+First, install npm packages (we assume you have pre-installed [node.js](https://nodejs.org/)).
+
+```bash
+npm install
+```
+
+## Commands
+
+After you installed packages, these commands are available in `package.json`.
+
+```bash
+node server.js # run in development mode
+```
+Navigate to ``` http://localhost:3000/ ```
--- server.js
+++ server.js
@@ -1,75 +1,96 @@
 const express = require('express');
+const session = require('express-session');
+const path = require('path');
+const crypto = require("crypto");
 const bodyParser = require('body-parser');
-const csurf = require('csurf');
 const cookieParser = require('cookie-parser');
 
 const PORT = process.env.PORT || 3000;
+
 const app = express();
 
-const csrfMiddleware = csurf({
-    cookie: true
-});
+app.set('views', path.join(__dirname, '/public'));
+app.set('view engine', 'ejs');
+app.use(express.static(__dirname + '/public/assets'));
+
+app.use(session({
+  name: 'sessionID',
+  secret: 'SLIITSSDKt2HA454tYPW',
+  resave: false,
+  saveUninitialized: true,
+  cookie: { secure: false, httpOnly: true }
+}))
+// Change cookie: { secure: true, httpOnly: true } when deploying to Production environment
 
 app.use(bodyParser.urlencoded({
-    extended: true
+  extended: true
 }));
+
 app.use(cookieParser());
-app.use(csrfMiddleware);
 
-app.get('/', (req, res) => {
-    res.send(`
-    <h1>Registration</h1>
-<form>
-    <div>
-        <label for="message">Username</label>
-        <input id="message" name="message" type="text" />
-        <label for="pw">Password</label>
-        <input id="pw" name="pw" type="text" />
-        <input type="button" value="Submit" onclick="onSubmitClick()" />
-    </div>
-    <div>
-        <input id="csrf_tocken" type="hidden" name="_csrf" value="${req.csrfToken()}" />
-    </div>
-</form>
-<script type="text/javascript" src="http://code.jquery.com/jquery-1.7.1.min.js"></script>
-<script type="text/javascript">
-    function onSubmitClick() {
-        var data = {
-            username: document.getElementById("message").value,
-            password: document.getElementById("pw").value
-        }
-        var csrfTocken = document.getElementById("csrf_tocken").value;
-        $.ajax({
-            url: 'http://localhost:3000/entry',
-            dataType: 'text',
-            type: 'post',
-            data: data,
-            headers: {
-                'X-CSRF-Token': csrfTocken
-            },
-            success: function (data, textStatus, jQxhr) {
-                console.log('errorThrown');
-            },
-            error: function (jqXhr, textStatus, errorThrown) {
-                console.log(errorThrown);
-            }
-        });
-    }
-</script>`
-    );
+// Views
+app.get('/', function (req, res) {
+  res.render('views/index', { msg: "", className: "" });
+});
+app.get('/form', function (req, res) {
+  res.render('views/form');
 });
 
-app.post('/entry', (req, res) => {
-    if (req.body.username == 'admin' && req.body.password == '123') {
-        console.log('Authenticate successfull');
-        res.send(`CSRF token used: ${req.cookies._csrf}, Message received: ${req.body.username}`);
-    }
-    else {
-        console.log('Authentication failed');
-        res.send(`CSRF token used: ${req.cookies._csrf}, Message received: ${req.body.message}`);
-    }
+
+app.get('/token', function (req, res) {
+  res.json(req.session.csrf);
 });
+
+app.post('/login', function (req, res) {
+  if (req.body.username == "ssd" && req.body.password == "ssd123") {
+    res.cookie('username', req.body.username);
+
+    let token = generateTocken();
+    req.session.csrf = token;
+    res.redirect('form');
+  } else {
+    res.render('views/index', {
+      msg: 'Invalid credentials! Please use default username and password.',
+      className: 'message-fail'
+    });
+  }
+})
+
+app.post('/formsubmit', function (req, res) {
+  if (req.session.csrf == req.body._csrf) {
+    res.render('views/message', {
+      msgTxt: 'Contact details saved successfully!',
+      reason: 'CSRF token is valid!',
+      className: 'success'
+    });
+  } else {
+    res.render('views/message', {
+      msgTxt: 'Invalid Contact Data.',
+      reason: 'Invalid CSRF Tocken !!!',
+      className: 'fail'
+    });
+  }
+})
+
+var length = 50;
+var timestamp = +new Date();
+function getRandomInt(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function generateTocken() {
+  var ts = timestamp.toString();
+  var parts = ts.split("").reverse();
+  var id = "";
+  for (var i = 0; i < length; ++i) {
+    var index = getRandomInt(0, parts.length - 1);
+    id += parts[index];
+  }
+  var createdTocken = crypto.createHash('sha256').update(id).digest('hex');
+  return createdTocken;
+}
 
 app.listen(PORT, () => {
-    console.log(`Listening on http://localhost:${PORT}`);
+  console.log(`Listening on http://localhost:${PORT}`);
 });
+
